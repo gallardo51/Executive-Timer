7c15995ebc54d08131f9297ef7c70aefa13881f1
Alex Sobolev <31108@mail.ru>
2023-11-26 17:03:12 +0300

    Setup code

--- Executive Timer/ContentView.swift
+++ Executive Timer/ContentView.swift
@@ -7,18 +7,65 @@
 
 import SwiftUI
 
+let defaultTime: CGFloat = 20
+let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
 struct ContentView: View {
+    
+    @State private var timeRunning = false
+    @State private var countdownTime: CGFloat = defaultTime
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        let strokeStyle = StrokeStyle(lineWidth: 15, lineCap: .round)
+        
+        let buttonIcon = timeRunning ? "pause.rectangle.fill" : "play.rectangle.fill"
+        
+        let countdownColor: Color = {
+            switch (countdownTime) {
+            case 6...: return Color.green
+            case 3...: return Color.yellow
+            default: return Color.red
+            }
+        } ()
+        
+        ZStack {
+            Circle()
+                .stroke(Color.gray.opacity(0.2), style: strokeStyle)
+            Circle()
+                .trim(from: 0, to: 1 - ((defaultTime - countdownTime) / defaultTime))
+                .stroke(countdownColor, style: strokeStyle)
+                .rotationEffect(.degrees(-90))
+                .animation(.easeInOut, value: 0.5)
+            HStack(spacing: 25) {
+                Label("", systemImage: buttonIcon)
+                    .foregroundColor(.black).font(.title)
+                    .onTapGesture (
+                        perform: { timeRunning.toggle() })
+                Text("\(Int(countdownTime))")
+                    .font(.largeTitle)
+                Label("", systemImage: "gobackward")
+                    .foregroundColor(.red)
+                    .font(.title)
+                    .onTapGesture (perform: {
+                        timeRunning = false
+                        countdownTime = defaultTime
+                    })
+            } .frame(width: 150 * 2, height: 150 * 2)
+                .onReceive(timer, perform: { _ in
+                    guard timeRunning else { return }
+                    if countdownTime > 0 {
+                        countdownTime -= 1
+                    } else {
+                        timeRunning = false
+                        countdownTime = defaultTime
+                    }
+                })
         }
-        .padding()
     }
 }
-
+    
 #Preview {
-    ContentView()
-}
+            ContentView()
+        }
