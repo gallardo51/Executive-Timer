7cc924a93b47c57929e78dc1e35b718b13f08475
Alex Sobolev <31108@mail.ru>
2023-11-26 15:42:21 +0300

    Initial Commit

---
+++ Executive Timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Executive Timer
+//
+//  Created by Александр Соболев on 26.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Executive Timer/Executive_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Executive_TimerApp.swift
+//  Executive Timer
+//
+//  Created by Александр Соболев on 26.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Executive_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
